e4173e65dc994292751df2f58528ea9504b969de
Ahmed-Ramadan-8513 <ahmedramadan8513@gmail.com>
2024-04-15 16:58:03 +0200

    Upload Files

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Hero from './components/Hero/Hero';
+import HomeCollection from './components/HomeCollection/HomeCollection';
+import HomeSale from './components/HomeSale/HomeSale';
+import HomeServices from './components/HomeServices/HomeServices';
+import HomeProducts from './components/HomeProducts/HomeProducts';
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Hero />
+            <HomeCollection />
+            <HomeServices />
+            <HomeSale />
+            <HomeProducts />
+       
+        </div>
+    )
+}
+
+export default Home;
